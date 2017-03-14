6af8e3d8bdbc646e4ac282b4a504ccdcd34d7348
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-03-14 12:42:10 +0100

    Add Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('scripts', function() {
+  return gulp.src(['./src/assets/js/vendor/jquery.min.js', './src/dist/js/bootstrap.js', './src/assets/js/docs.min.js'])
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./src/dist/js/'));
+});
